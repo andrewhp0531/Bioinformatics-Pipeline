7e93bc135f892de5c39d1bcc48d9386e48756eb8
andrewhp0531 <luckyacorn7@gmail.com>
2026-06-30 16:19:01 +0900

    Background Knowledge

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+import time
+
+def main():
+    # print("Hello from bioinformatics-pipeline!")
+    # print(sys.executable)
+    while True: 
+        print('Testing')
+        time.sleep(2)
+
+
+if __name__ == "__main__":
+    main()
